8aa21c239d007fdc94d5334ac98407d20fe0ef92
sadbc1235 <sadbc1235@naver.com>
2024-04-03 16:41:35 +0900

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import { Controller, Get, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return this.appService.uploadFile(file);
+  }
+}
